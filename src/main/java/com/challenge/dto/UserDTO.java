36672f9672dbaa9ad60c4b130120f57e169119c3
alan <abenitez@pigmalion.com.ar>
2023-07-12 19:01:28 -0300

    Se ajusta el dto para updatear correctamente y se coloca como unique el dni y el email.

--- src/main/java/com/challenge/dto/UserDTO.java
+++ src/main/java/com/challenge/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.challenge.dto;
 
 import com.challenge.entities.Course;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDTO {
 
     private Long dni;
