84c3a07f715a7e27171d3a6702c478e71d17d6b3
alan <abenitez@pigmalion.com.ar>
2023-07-12 23:23:57 -0300

    Se añaden clases Response y ResponseCode para homogeneizar las respuestas.

---
+++ src/main/java/com/challenge/out/ResponseCode.java
@@ -0,0 +1,24 @@
+package com.challenge.out;
+
+public enum ResponseCode {
+    SUCCESS("Ok", 200),
+    FAILURE("Fail", 400),
+    NOT_FOUND("Resource not found", 404);
+
+
+    private String text;
+    private int statusCode;
+
+    ResponseCode(String text, int statusCode) {
+        this.text = text;
+        this.statusCode = statusCode;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public int getStatusCode() {
+        return this.statusCode;
+    }
+}
